cdef17c382db4b4d4b6c7c330788163926840555
vladimiruzunov <uzunovladimir@gmail.com>
2021-01-31 09:35:10 +0200

    +

---
+++ AsociativeArrays/TextFilter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TextFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] bannedWords = scanner.nextLine().split(", ");
+        String text = scanner.nextLine();
+
+        for (String bannedWord : bannedWords) {
+            text = text.replace(bannedWord, repeat("*",bannedWord.length()));
+        }
+        System.out.println(bannedWords);
+
+    }
+    private static String repeat(String word, int count){
+        String[] repeatArr = new String[count];
+        for (int i = 0; i < count; i++) {
+           repeatArr[i] = word;
+        }
+        return String.join("", repeatArr);
+    }
+}
