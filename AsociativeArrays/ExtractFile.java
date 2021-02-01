5c844d9a6d30f899ae17bb2dda013e95acacfebc
vladimiruzunov <uzunovladimir@gmail.com>
2021-02-01 18:23:20 +0200

    +

---
+++ AsociativeArrays/ExtractFile.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ExtractFile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String path = scanner.nextLine();
+        String file = path.substring(path.lastIndexOf("\\")+ 1);
+        String name = file.substring(0, file.indexOf("."));
+        String extension = file.substring(file.indexOf(".") + 1);
+        System.out.println("File name: " + name);
+        System.out.println("File extension: " + extension);
+    }
+}
